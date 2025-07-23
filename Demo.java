3f0ba7025f1b37223b07659927be720e864dce99
Vishnu Vardan <vian.codes@gmail.com>
2025-07-23 17:25:40 -0400

    My update

---
+++ Demo.java
@@ -0,0 +1,42 @@
+public class Demo 
+{
+    public static void main(String args[]) {
+        // int x = 7;
+        // int y = 15;
+        // int z=100;
+
+        // // int b=9;
+
+        // if(x>y && x>z){
+        //     // boolean result = x>y && a<b;
+        //     // boolean result = x>y || a<b;
+        //     System.out.println(x);
+        // }
+        // else if(y>x && y>z)
+        //     System.out.println(y);
+        // else if(z>x && z>y)
+        //     System.out.println(z);
+
+        int n,result;
+
+        n=5;
+        result=0;
+
+        String s=""; 
+
+
+        result= (n%2==0)? 10:20;
+
+
+        if(result==10)
+            System.out.println("even");
+
+        else if(result == 20)
+            System.out.println("odd");
+
+
+
+
+
+    }
+}
---
+++ World.java
@@ -0,0 +1,13 @@
+public class World{
+    public static void main(String[] args){
+        int num1=3;
+        // int num2=8;
+        
+        // int result= num1 + num2;
+        // System.out.println("Hello, World!");
+
+
+        num1 = num1 + 5;
+        System.out.println(num1);
+    }
+}
